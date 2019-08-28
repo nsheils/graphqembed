bedfa1638f8316ee791835936dd20e5976c50c25
Natalie <nsheils@users.noreply.github.com>
2019-08-28 12:22:41 -0500

    added list() around .values() to work with Python3

--- netquery/train_helpers.py
+++ netquery/train_helpers.py
@@ -53,7 +53,7 @@ def run_train(model, optimizer, train_queries, val_queries, test_queries, logger
             logger.info("Edge converged at iteration {:d}".format(i-1))
             logger.info("Testing at edge conv...")
             conv_test = run_eval(model, test_queries, i, logger)
-            conv_test = np.mean(conv_test.values())
+            conv_test = np.mean(list(conv_test.values()))
             edge_conv = True
             losses = []
             ema_loss = None
@@ -84,13 +84,13 @@ def run_train(model, optimizer, train_queries, val_queries, test_queries, logger
         if i >= val_every and i % val_every == 0:
             v = run_eval(model, val_queries, i, logger)
             if edge_conv:
-                vals.append(np.mean(v.values()))
+                vals.append(np.mean(list(v.values())))
             else:
                 vals.append(v["1-chain"])
     
     v = run_eval(model, test_queries, i, logger)
-    logger.info("Test macro-averaged val: {:f}".format(np.mean(v.values())))
-    logger.info("Improvement from edge conv: {:f}".format((np.mean(v.values())-conv_test)/conv_test))
+    logger.info("Test macro-averaged val: {:f}".format(np.mean(list(v.values()))))
+    logger.info("Improvement from edge conv: {:f}".format((np.mean(list(v.values()))-conv_test)/conv_test))
 
 def run_batch(train_queries, enc_dec, iter_count, batch_size, hard_negatives=False):
     num_queries = [float(len(queries)) for queries in train_queries.values()]
